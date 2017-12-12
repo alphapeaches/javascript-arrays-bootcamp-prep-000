7b1789fae661b7a9efae685f3bcdac5edfbbf8df
Rachel Acosta <denversunrise@gmail.com>
2017-12-12 20:41:32 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -10,13 +10,24 @@ var ingredient6 = "garlic";
 //var tomatoSauceIngredients = ['tomato', 'garlic', 'olive oil', 'basil', 'oregano'];
 
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
 function addElementToBeginningOfArray(array, element){
   array = [element, ...array];
   return array;
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
-    array = array.unshift(element);
+    array.unshift(element);
+  return array;
+}
+
+function addElementToEndOfArray(array, element){
+  array = [...array, element];
+  return array;
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+    array[array.length] = element;
   return array;
 }
 
